dbbbac0f43b29dc4cca33c269f2389d565f99dc9
Imran Sharief <imran.shariefmca@gmail.com>
2026-08-24 14:57:40 +0530

    Refactor application entry point into main function

--- main.py
+++ main.py
@@ -1,30 +1,51 @@
 from menu import show_menu
-from task_manager import display_tasks,add_task,delete_task,edit_task,mark_task_completed,search_task
-# from task_manager import *
-from storage import load_tasks,save_tasks
 
-tasks=load_tasks()
+from task_manager import (
+    display_tasks,
+    add_task,
+    delete_task,
+    edit_task,
+    mark_task_completed,
+    search_task,
+)
 
-while True:
-    show_menu()
-    choice = input("Enter your choice: ").strip()
+from storage import load_tasks, save_tasks
 
-    if choice == "1":
-        display_tasks(tasks)
-    elif choice =="2":
-        add_task(tasks)
-        save_tasks(tasks)
-    elif choice =="3":
-        delete_task(tasks)
-        save_tasks(tasks)
-    elif choice =="4":
-        edit_task(tasks)
-        save_tasks(tasks)
-    elif choice == "5":
-         mark_task_completed(tasks)
-         save_tasks(tasks)
-    elif choice == "6":
-          search_task(tasks)
-    elif choice =="7":
-          print("Good Bye")
-          break
+
+def main():
+    tasks = load_tasks()
+
+    while True:
+        show_menu()
+        choice = input("Enter your choice: ").strip()
+
+        if choice == "1":
+            display_tasks(tasks)
+
+        elif choice == "2":
+            add_task(tasks)
+            save_tasks(tasks)
+
+        elif choice == "3":
+            delete_task(tasks)
+            save_tasks(tasks)
+
+        elif choice == "4":
+            edit_task(tasks)
+            save_tasks(tasks)
+
+        elif choice == "5":
+            mark_task_completed(tasks)
+            save_tasks(tasks)
+
+        elif choice == "6":
+            search_task(tasks)
+
+        elif choice == "7":
+            print("Good Bye")
+            break
+
+
+
+if __name__== "__main__":
+    main()
